de89127624e26290b6af7cdec40fd209df7a8165
valdirpn <valdiraires.pn@gmail.com>
2021-10-20 21:56:46 -0300

    feature: input and select components

---
+++ src/components/Input.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export default styled.input`
+  width: 100%;
+  height: 52px;
+  background: #fff;
+  border: 2px solid #fff;
+  border-radius: 4px;
+  outline: none;
+
+  padding: 0 16px;
+  font-size: 16px;
+
+  transition: .2s ease-in;
+
+  &:focus {
+    border-color: ${({ theme }) => theme.colors.primary.main};
+  }
+`;
--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,7 +1,17 @@
+import Input from "../../components/Input";
 import PageHeader from "../../components/PageHeader";
+import Select from "../../components/Select";
 
 export default function NewContact() {
   return (
-    <PageHeader title="Novo contato" />
+    <>
+      <PageHeader title="Novo contato" />
+      <Input type="text" placeholder="Nome" />
+      <Select>
+        <option value="1">Instagram</option>
+        <option value="2">Linkedin</option>
+        <option value="3">Twitter</option>
+      </Select>
+    </>
   );
 }
